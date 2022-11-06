12e073f28c7b755d9e974b95a32491caa168c4ef
dalijovorkapic <dv52809@fer.hr>
2022-11-06 13:11:17 +0100

    server

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const path = require('path');
 
 const app = express();
 
+const externalUrl = process.env.RENDER_EXTERNAL_URL;
+const port = externalUrl && process.env.PORT ? parseInt(process.env.PORT) : 4080;
+const hostname = '127.0.0.1';
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/lab2'));
 
@@ -11,4 +14,5 @@ app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/lab2'}),
 );
 
-app.listen(process.env.PORT || 4080);
+app.listen(port, hostname);
+
